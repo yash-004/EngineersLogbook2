f9a842a9e76ef7654edbf44ab8b3222d6bbe5d76
lewis <lewistham9x@gmail.com>
2020-07-29 13:00:11 +0800

    .gitignore fix

--- node_modules/cordova-plugin-fcm-with-dependecy-updated/src/android/com/gae/scaffolder/plugin/interfaces/OnFinishedListener.java
+++
@@ -1,7 +0,0 @@
-package com.gae.scaffolder.plugin.interfaces;
-
-import android.support.annotation.NonNull;
-
-public interface OnFinishedListener<TResult> {
-    void success(@NonNull TResult result);
-}
