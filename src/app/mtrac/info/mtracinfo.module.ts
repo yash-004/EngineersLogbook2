95be8e090c0549021f34f0905a18c5ccdf421ebf
lewis <lewistham9x@gmail.com>
2020-10-19 13:21:19 +0800

    Removed signature pad, added mtrac info

---
+++ src/app/mtrac/info/mtracinfo.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { mtracinfoPage } from './mtracinfo.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: mtracinfoPage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    ReactiveFormsModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [mtracinfoPage]
+})
+export class mtracinfoPageModule {}
